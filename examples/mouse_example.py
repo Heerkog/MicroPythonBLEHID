fd0ff95a5478e2cca7013fe52874119e51ce611d
Heerkog <heerkogroefsema@gmail.com>
2021-03-29 17:01:44 +0200

    Licensing

--- examples/mouse_example.py
+++ examples/mouse_example.py
@@ -1,3 +1,20 @@
+# MicroPython Human Interface Device library
+# Copyright (C) 2021 H. Groefsema
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 # Implements a BLE HID mouse
 import time
 from machine import SoftSPI, Pin
@@ -120,5 +137,3 @@ class Device:
 
 if __name__ == "__main__":
     d = Device()
-    d.start()
-    
